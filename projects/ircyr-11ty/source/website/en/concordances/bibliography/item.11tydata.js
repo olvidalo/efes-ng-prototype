6c5302525b44c85c452ed0dfbe8074e3d6cfe288
Marcel Schaeben <m.schaeben@uni-koeln.de>
2026-04-17 14:25:06 +0200

    remove example projects from repo

--- projects/ircyr-11ty/source/website/en/concordances/bibliography/item.11tydata.js
+++
@@ -1,8 +0,0 @@
-// Pagination: generates one page per bibliography entry.
-module.exports = {
-    pagination: {
-        data: "indices.bibliography.entries",
-        size: 1,
-        alias: "item",
-    }
-};
